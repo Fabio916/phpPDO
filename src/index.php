fe552091d4cbb6270360e22496b95b5db939edad
Fabio916 <ferreirafaabio04@gmail.com>
2023-05-29 20:43:52 -0300

    PDO

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>começcando com PDO</h1>
+    <h2>PDO é a tecnologia </h2>
+</body>
+</html>
